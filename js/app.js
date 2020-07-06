19c93b7f51c0af879d3068e6e186139dede8b590
Connor Twele <connortwele@gmail.com>
2020-07-06 18:13:11 -0400

    homework

---
+++ js/app.js
@@ -0,0 +1,88 @@
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var ten = 10;
+
+console.log(one += two);
+console.log(three += four);
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var ten = 10;
+
+console.log(five -= one);
+console.log(nine -= six);
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var ten = 10;
+
+console.log(four *= eight);
+console.log(ten *= three);
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var ten = 10;
+
+console.log(nine /= three);
+console.log(eight /= four);
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var ten = 10;
+
+
+one++
+console.log(one);
+ten++
+console.log(ten);
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var ten = 10;
+
+two--
+console.log(two);
+five--
+console.log(five);
